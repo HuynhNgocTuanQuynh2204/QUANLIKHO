8f22365f59e05ccb4ac546b4682afc4724a203c4
HuynhNgocTuanQuynh2204 <tuanquynh3452@gmail.com>
2024-05-23 19:44:20 +0700

    first name

--- pages/xuly/xulysuahoso.php
+++ pages/xuly/xulysuahoso.php
@@ -1,29 +1,36 @@
 <?php
- require("carbon/autoload.php");
- use Carbon\Carbon;
- use Carbon\CarbonInterval;
- $now = Carbon::now('Asia/Ho_Chi_Minh');
- $now->format('Y-m-d H:i:s');
-  $tenql = $_POST['hovaten'];
-  $matkhau = $_POST['password'];
-  $avatar = $_FILES['avatar']['name'];
-  $avatar_tmp = $_FILES['avatar']['tmp_name'];
-  $avatar_time = time().'_'.$avatar;
- if(isset($_POST['suahoso'])){
-    //sua
-    if($avatar !=''){
-      move_uploaded_file($avatar_tmp,'images/avatar/'.$avatar_time);       
-      $sql_update = "UPDATE user SET hovaten='". $tenql."',password ='". $matkhau."',  avatar='". $avatar_time."' WHERE id_user='$_GET[id]'";
-      $sql = "SELECT * FROM user WHERE id_user = '$_GET[id]' LIMIT 1";
-      $query = mysqli_query($mysqli,$sql);
-      while($row = mysqli_fetch_array($query)){
-         unlink('avatar/'.$row['avatar']);
-      }
-   }else{
-      $sql_update = "UPDATE user SET hovaten='". $tenql."',password ='". $matkhau."' WHERE id_user='$_GET[id]'";
-   }
-   mysqli_query($mysqli,$sql_update);
-    echo '<script type="text/javascript">alert("Sửa thông tin thành công");    window.location.href = "index.php?quanly=hoso"; </script>';
+require("carbon/autoload.php");
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
 
- }
+$now = Carbon::now('Asia/Ho_Chi_Minh');
+$now->format('Y-m-d H:i:s');
+
+$tenql = $_POST['tenql'];
+$matkhau = $_POST['matkhau'];
+$avatar = $_FILES['avatar']['name'];
+$avatar_tmp = $_FILES['avatar']['tmp_name'];
+$avatar_time = time() . '_' . $avatar;
+
+if (isset($_POST['suahoso'])) {
+    // Mã hóa mật khẩu
+    $hashed_password = password_hash($matkhau, PASSWORD_DEFAULT);
+
+    if ($avatar != '') {
+        move_uploaded_file($avatar_tmp, 'images/avatar/' . $avatar_time);
+        $sql_update = "UPDATE user SET hovaten='" . $tenql . "',password='" . $hashed_password . "', avatar='" . $avatar_time . "' WHERE id_user='$_GET[id]'";
+        $sql = "SELECT * FROM user WHERE id_user = '$_GET[id]' LIMIT 1";
+        $query = mysqli_query($mysqli, $sql);
+        while ($row = mysqli_fetch_array($query)) {
+            unlink('images/avatar/' . $row['avatar']);
+        }
+    } else {
+        $sql_update = "UPDATE user SET hovaten='" . $tenql . "',password='" . $hashed_password . "' WHERE id_user='$_GET[id]'";
+    }
+    mysqli_query($mysqli, $sql_update);
+    echo '<script type="text/javascript">
+            alert("Sửa thông tin thành công");
+            window.location.href = "index.php?quanly=hoso";
+          </script>';
+}
 ?>
